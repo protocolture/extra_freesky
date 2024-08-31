f25e5640de5d4cb5bfc741d27884b46f55d990be
Matt Osborne <osbornematt1@gmail.com>
2024-08-31 13:23:40 +1000

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-repo_url = 'https://github.com/protocolture/extra_freesky'
-clone_dir=~/repo
-launch_script_path=~/repo/launch/launch_script.sh
+repo_url="https://github.com/protocolture/extra_freesky"
+clone_dir="~/repo"
+launch_script_path="~/repo/launch/launch_script.sh"
 
 
 # Step 1: Pull the GitHub repository
@@ -12,11 +12,21 @@ if [ ! -d "$clone_dir" ]; then
     echo "Directory $clone_dir does not exist. Creating it..."
     mkdir -p "$clone_dir"
 else
-    echo "Directory $clone_dir already exists."
+    echo "Directory $clone_dir already exists. Removing."
+	rm -d -r "$clone_dir"
+	mkdir -p "$clone_dir"
 fi
 
 echo "Cloning the repository into $clone_dir..."
-git clone "$repo_url" "$clone_dir"
+git clone $repo_url $clone_dir
+
+chmod +x $launch_script_path
+
+# Erroor Checking
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to clone repository. Please check the URL and your network connection. Exiting."
+    exit 1
+fi
 
 # Step 2: Set the environment variable with options
 echo "Select the mode for Extra_Freesky:"
@@ -53,9 +63,9 @@ source ~/.bashrc
 echo "Setting up launch script to run at startup..."
 
 # Create a systemd service file to run the launch script at startup
-sudo bash -c 'cat << EOF > /etc/systemd/system/launch_script.service
+sudo bash -c 'cat << EOF > /etc/systemd/system/freesky.service
 [Unit]
-Description=Launch Script at Startup
+Description=Launch Freesky
 
 [Service]
 ExecStart='$launch_script_path'
@@ -63,12 +73,12 @@ Environment="EXTRA_FREESKY='$env_var_value'"
 StandardOutput=journal
 StandardError=journal
 Restart=always
-User='$USER'
+User='$USER'cat
 WorkingDirectory='$HOME/repo'
 [Install]
 WantedBy=multi-user.target
 EOF'
 
 # Enable the systemd service
-sudo systemctl enable launch_script.service
+sudo systemctl enable freesky.service
 echo "Installation complete. The extra_freesky script will run at startup."
